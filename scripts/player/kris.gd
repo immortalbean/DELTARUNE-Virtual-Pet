179e84ef3df865101c96c02d0d81e56e6fefc5f2
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-09-27 20:14:14 -0400

    added hatless ralsei

--- scripts/player/kris.gd
+++ scripts/player/kris.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const base_speed_light_world: float = 420.0
 const run_addon_1: float = 240.0
 
-const max_form: int = 4
+const max_form: int = 5
 
 var moving = false
 
--- scripts/window_manager.gd
+++ scripts/window_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var mouse_offset: Vector2 = Vector2.ZERO
 var dragging = false
 var kris: CharacterBody2D
+var time: float = 0.0
 
 func _ready() -> void:
 	var _rid = get_tree().get_root().get_viewport_rid()
@@ -10,7 +11,8 @@ func _ready() -> void:
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT,true)
 	
 	kris = get_parent()
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	time += delta
 	if Input.is_action_just_pressed("mouse_click"):
 		mouse_offset = kris.position - kris.get_global_mouse_position()
 		dragging = true
